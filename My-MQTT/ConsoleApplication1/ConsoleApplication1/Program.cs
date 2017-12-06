cd4c07794d9467a3c2ecb88ce555e8ef0bbdf0f0
ZouHeng <zouheng_zhhe@163.com>
2017-12-06 14:14:52 +0800

    添加之前开发的MQTT客户端（该客户端用于发送消息）

---
+++ My-MQTT/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建客户端实例  
+            //MqttClient client = new MqttClient(IPAddress.Parse(MQTT_BROKER_ADDRESS)); //主机为IP时  
+            //MqttClient client = new MqttClient(MQTT_BROKER_ADDRESS); //当主机地址为域名时  
+
+            MqttClient client = new MqttClient(IPAddress.Parse("127.0.0.1"));
+
+            // 注册消息接收处理事件，还可以注册消息订阅成功、取消订阅成功、与服务器断开等事件处理函数  
+            //client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            //生成客户端ID并连接服务器  
+            //string clientId = Guid.NewGuid().ToString();
+            //client.Connect("aaa");
+            //client.Connect("127.0.0.1");
+            //Console.WriteLine(client.IsConnected);
+            try
+            {
+                client.Connect("MJ_124804073");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(client.IsConnected);
+
+
+            // 订阅主题"/home/temperature" 消息质量为 2   
+            //client.Subscribe(new string[] { "angular" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+            //client.Publish("angular", System.Text.Encoding.Default.GetBytes("Hello World"));
+            client.Publish("angular", Encoding.UTF8.GetBytes("hello"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            Console.ReadKey();
+        }
+
+      
+
+       static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            //处理接收到的消息  
+            string msg = System.Text.Encoding.Default.GetString(e.Message);
+
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ My-MQTT/ConsoleApplication1/mqttsend/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace mqttsend
+{
+    public partial class Form1 : Form
+    {
+        private MqttClient client = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //这个字符串是向服务器发送的数据信息
+            string strValue = "1,1,1,1,1,1";
+            // 发送一个内容是123 字段是klabs的信息
+            client.Publish("angular", Encoding.UTF8.GetBytes(strValue));
+            //MessageBox.Show("发送消息的消息是："+strValue);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new MqttClient(IPAddress.Parse("127.0.0.1"));
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            client.Connect("send");
+            //client.Subscribe(new string[] { "angular" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+        }
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            //处理接收到的消息
+            string msg = System.Text.Encoding.Default.GetString(e.Message);
+            //label1.Text = msg;
+            MessageBox.Show("接受到的消息是：" + msg);
+            Console.WriteLine(msg);
+        }
+    }
+}
